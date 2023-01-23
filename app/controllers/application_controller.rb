5970b0ad9167c99d491921e6c067f57e0d180d94
syarif.ibrohim <syarif.ibrohim@bukalapak.com>
2023-01-23 08:41:25 +0700

    auth controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+  RSA_PRIVATE = OpenSSL::PKey::RSA.generate 2048
+  RSA_PUBLIC = RSA_PRIVATE.public_key
 end
---
+++ app/controllers/authentications_controller.rb
@@ -0,0 +1,20 @@
+class AuthenticationsController < ApplicationController
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user&.authenticate(params[:password])
+      render :json => { username: @user.username, token: generate_token, exp: payload[:exp] }, :status => :ok
+    else
+      render :json => { error: "You are not authorized to access this resource" }, :status => :unauthorized
+    end
+  end
+
+  private
+
+  def payload
+    { resource_owner_id: @user.id, exp: Time.now.to_i + 10 }
+  end
+
+  def generate_token
+    JWT.encode payload, RSA_PRIVATE, 'RS256'
+  end
+end
